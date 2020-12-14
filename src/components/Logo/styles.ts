34f5d2012ce6a2a12c5a1960d7038f2666d0cd5b
RiczCalixto <rcalixtos@gmail.com>
2020-12-14 14:07:09 -0300

    feat: add size and hideOnMobile props at logo component

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,12 +1,46 @@
 import styled, { css, DefaultTheme } from 'styled-components'
+import media from 'styled-media-query'
+
+export type Color = 'white' | 'black'
+export type Size = 'normal' | 'large'
 
 interface WrapperProps {
-  color: 'white' | 'black'
+  color: Color
+  size: Size
   theme: DefaultTheme
+  hideOnMobile: boolean
+}
+
+const wrapperModifies = {
+  normal: () => css`
+    width: 11rem;
+    height: 3.3rem;
+  `,
+  large: () => css`
+    width: 20rem;
+    height: 5.9rem;
+  `,
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+     width: 5.8rem;
+     height: 4.5rem;
+
+     svg {
+       height: 4.5rem;
+       pointer-events: none;
+     }
+
+     .text{
+       display: none;
+     }
+    `}
+  `
 }
 
 export const Wrapper = styled.div<WrapperProps>`
-  ${({ color, theme }) => css`
+  ${({ color, theme, size, hideOnMobile }) => css`
     color: ${theme.colors[color]};
+    ${!!size && wrapperModifies[size]}
+    ${!!hideOnMobile && wrapperModifies.hideOnMobile}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -17,4 +17,29 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a normal Logo by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games Logo/i).parentElement).toHaveStyle({
+      width: '11rem',
+      height: '3.3rem'
+    })
+  })
+
+  it('should render a bigger Logo', () => {
+    renderWithTheme(<Logo size={'large'} />)
+    expect(screen.getByLabelText(/Won Games Logo/i).parentElement).toHaveStyle({
+      width: '20rem',
+      height: '5.9rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(
+      screen.getByLabelText(/Won Games Logo/i).parentElement
+    ).toHaveStyleRule('width', '5.8rem', {
+      media: '(max-width: 768px)'
+    })
+  })
 })
