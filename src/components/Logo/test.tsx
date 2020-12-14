1a56daa49caf2519e8fa99de8244cda65db842dd
RiczCalixto <rcalixtos@gmail.com>
2020-12-14 12:53:51 -0300

    feat: add test to Logo component

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white Logo by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games Logo/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a black Logo when color props is passed', () => {
+    renderWithTheme(<Logo color={'black'} />)
+    expect(screen.getByLabelText(/Won Games Logo/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
