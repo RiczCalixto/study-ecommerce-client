972064ca4d17d064fbb9ea4b56d7467f9341d13c
RiczCalixto <rcalixtos@gmail.com>
2020-12-14 19:42:47 -0300

    feat: add heading style

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -4,15 +4,17 @@ import * as S from './styles'
 export interface HeadingProps {
   color?: Color
   lineLeft?: boolean
+  lineBottom?: boolean
   children: React.ReactNode
 }
 
 export const Heading: React.FC<HeadingProps> = ({
   color = 'black',
   lineLeft = false,
+  lineBottom = false,
   children
 }) => (
-  <S.Wrapper color={color} lineLeft={lineLeft}>
+  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
     {children}
   </S.Wrapper>
 )
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,26 +1,39 @@
 import { Color } from 'model/common-style-types'
 import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
-
 interface WrapperProps {
   color: Color
   lineLeft: boolean
+  lineBottom: boolean
 }
-
 const wrapperModifiers = {
   lineLeft: (theme: DefaultTheme) => css`
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors.secondary};
+  `,
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::after {
+      position: absolute;
+      left: 0;
+      bottom: -1rem;
+      content: '';
+      width: 5rem;
+      border: 0.4rem solid ${theme.colors.primary};
+    }
   `
 }
 
 export const Wrapper = styled.h2<WrapperProps>`
-  ${({ theme, color, lineLeft }) => css`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
+    color: ${theme.colors[color]};
     font-size: ${theme.font.sizes.xlarge};
     ${media.greaterThan('medium')`
-    font-size: ${theme.font.sizes.xxlarge};
+      font-size: ${theme.font.sizes.xxlarge};
     `}
-    color: ${theme.colors[color]}
-    ${lineLeft && wrapperModifiers.lineLeft(theme)}
+    ${lineLeft && wrapperModifiers.lineLeft(theme)};
+    ${lineBottom && wrapperModifiers.lineBottom(theme)};
   `}
 `
