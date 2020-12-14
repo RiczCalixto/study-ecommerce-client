71aef8b7b04b987d353030db0e4a21c9abbd4ed2
RiczCalixto <rcalixtos@gmail.com>
2020-12-13 22:17:58 -0300

    feat: add fonts and theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,16 @@
+import { ThemeProvider } from 'styled-components'
 import { addDecorator } from '@storybook/react'
 import { withNextRouter } from 'storybook-addon-next-router'
+import { theme } from '../src/styles/theme'
 import GlobalStyles from '../src/styles/global'
 
 addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
