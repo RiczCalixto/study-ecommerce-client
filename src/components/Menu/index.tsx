3a11ef2916208410aaec7dcd280885e97111cc36
RiczCalixto <rcalixtos@gmail.com>
2021-02-23 20:11:30 -0300

    feat: add menu test

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,33 @@
+import * as React from 'react'
 import * as S from './styles'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import { Logo } from 'components/Logo'
 
-export const Menu: React.FC = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-    <S.MenuGroup>
+export const Menu: React.FC = () => {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  return (
+    <S.Wrapper>
       <S.IconWrapper>
-        <ShoppingCartIcon />
+        <MenuIcon aria-label="Open menu" onClick={() => setIsOpen(true)} />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <SearchIcon />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
