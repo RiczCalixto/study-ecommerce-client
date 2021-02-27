4e5a612889737a86f9bb96464902eb3cbca63b45
RiczCalixto <rcalixtos@gmail.com>
2021-02-27 16:04:30 -0300

    feat: add menu desktop

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,44 @@
 import * as React from 'react'
 import * as S from './styles'
+import Link from 'next/link'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import { Logo } from 'components/Logo'
+import { Button } from 'components/Button'
+import { MediaMatch } from 'components/MediaMatch'
 
-export const Menu: React.FC = () => {
-  const [isOpen, setIsOpen] = React.useState(false)
+export interface MenuProps {
+  username?: string
+}
+
+export const Menu: React.FC<MenuProps> = ({ username }) => {
+  const [isVisible, setIsVisible] = React.useState(false)
+  const isLogged = !!username
 
   return (
     <S.Wrapper>
-      <S.IconWrapper>
-        <MenuIcon aria-label="Open menu" onClick={() => setIsOpen(true)} />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper>
+          <MenuIcon aria-label="Open menu" onClick={() => setIsVisible(true)} />
+        </S.IconWrapper>
+      </MediaMatch>
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
+          <Link href="/games" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
@@ -24,10 +46,59 @@ export const Menu: React.FC = () => {
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+        {!isLogged && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
-      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
-        <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
-      </S.MenuFull>
+
+      <MenuModal
+        isVisible={isVisible}
+        isLogged={isLogged}
+        setIsVisible={setIsVisible}
+      />
     </S.Wrapper>
   )
 }
+
+interface MenuModalProps {
+  isVisible: boolean
+  setIsVisible: (isOpen: boolean) => void
+  isLogged: boolean
+}
+
+const MenuModal = ({ isVisible, setIsVisible, isLogged }: MenuModalProps) => (
+  <S.MenuFull aria-hidden={!isVisible} isOpen={isVisible}>
+    <CloseIcon aria-label="Close menu" onClick={() => setIsVisible(false)} />
+    <S.MenuNav>
+      <Link href="/" passHref>
+        <S.MenuLink>Home</S.MenuLink>
+      </Link>
+      <Link href="/games" passHref>
+        <S.MenuLink>Explore</S.MenuLink>
+      </Link>
+      {isLogged && (
+        <>
+          <Link href="/" passHref>
+            <S.MenuLink>My account</S.MenuLink>
+          </Link>
+          <Link href="/games" passHref>
+            <S.MenuLink>Wishlist</S.MenuLink>
+          </Link>
+        </>
+      )}
+    </S.MenuNav>
+    {!isLogged && (
+      <S.RegisterBox>
+        <Button fullWidth size="large">
+          Log in now
+        </Button>
+        <span>or</span>
+        <Link href="#" passHref>
+          <S.CreateAccount>Sign Up</S.CreateAccount>
+        </Link>
+      </S.RegisterBox>
+    )}
+  </S.MenuFull>
+)
--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,9 +1,16 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { Menu } from '.'
+import { Menu, MenuProps } from 'components/Menu'
 
 export default {
   title: 'Menu',
   component: Menu
 } as Meta
 
-export const Component: Story = () => <Menu />
+export const Component: Story<MenuProps> = (args) => <Menu {...args} />
+
+Component.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
